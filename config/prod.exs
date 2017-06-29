1f4dae71b86f9ced3d11e17266208664102c7dc2
Sebastian Hoß <sebastian.hoss@cronn.de>
2017-06-29 22:16:49 +0200

    prod config refernces system env

--- config/prod.exs
+++ config/prod.exs
@@ -58,4 +58,12 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+#import_config "prod.secret.exs"
+
+config :kikr, Kikr.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: System.get_env("DB_USER"),
+  password: System.get_env("DB_PASS"),
+  database: System.get_env("DB_NAME"),
+  hostname: System.get_env("DB_HOST"),
+  pool_size: String.to_integer(System.get_env("DB_POOL") || "10"),
