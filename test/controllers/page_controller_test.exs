cde742231964595e2b9db8371a1f7ea07bd992ce
Sebastian Hoß <sebastian.hoss@cronn.de>
2017-06-30 19:38:48 +0200

    fix test case

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Kikr.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to kikr!"
   end
 end
