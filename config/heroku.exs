dcd7fddb2a2875e0fdece16398c1ac51f79d1f6b
Sebastian Hoß <mail@shoss.de>
2017-07-03 21:13:18 +0200

    add heroku config for postgres

---
+++ config/heroku.exs
@@ -0,0 +1,20 @@
+use Mix.Config
+
+#
+# heroku config
+#
+
+config :kikr, Kikr.Endpoint,
+  http: [port: {:system, "PORT"}],
+  url: [host: "example.com", port: 80],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Do not print debug messages in production
+config :logger, level: :info
+
+config :hello_phoenix, HelloPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
